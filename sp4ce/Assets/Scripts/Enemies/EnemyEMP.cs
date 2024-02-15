202b2da768858b0e632b840f77116a9971b02cf0
asher505 <222349U@mymail.nyp.edu.sg>
2024-02-15 15:08:00 +0800

    lightybulby🤣🤣

--- sp4ce/Assets/Scripts/Enemies/EnemyEMP.cs
+++ sp4ce/Assets/Scripts/Enemies/EnemyEMP.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyEMP : EnemyBase
 {
 
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float chaseSpeed = 10f;
 
-    private new enum State
+    private enum State
     {
         PATROL,
         BLAST,
@@ -14,26 +17,34 @@ public class EnemyEMP : EnemyBase
         CHASE
     }
 
-    private new State currentState;
+    private State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = walkSpeed;
+        agent = GetComponent<NavMeshAgent>();
+        currentState = State.PATROL;
+        currWaypoint = 0;
+        target = patrolWaypoints[currWaypoint];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FSM();
+        Move(target);
     }
 
     public override void FSM()
     {
         switch (currentState)
         {
-            case State.PATROL: 
-               
+            case State.PATROL:
+                target = patrolWaypoints[currWaypoint];
+
+                if (agent.remainingDistance < .75f)
+                    currWaypoint = Random.Range(0, patrolWaypoints.Count);
                 break;
             case State.BLAST:
 
