b7d696e3a39ec8b70c6599f2e79cb61a6314cfd5
andrewvil <220518Y@mymail.nyp.edu.sg>
2024-02-15 11:03:13 +0800

    mouse sensitivity

--- sp4ce/Assets/Scripts/CameraController.cs
+++ sp4ce/Assets/Scripts/CameraController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     float mouseX, mouseY;
+
+    [SerializeField]
+    private float mouseSensitivity = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +18,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mouseX += Input.GetAxis("Mouse X");
-        mouseY += -Input.GetAxis("Mouse Y");
+        mouseX += Input.GetAxis("Mouse X")* mouseSensitivity;
+        mouseY += -Input.GetAxis("Mouse Y") * mouseSensitivity;
         mouseY = Mathf.Clamp(mouseY, -89f, 89f);
         Camera.main.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
     }
