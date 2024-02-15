3361fd7c335cc47c933f7cfd715041be1a860307
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2024-02-15 14:26:25 +0800

    Updated door

--- sp4ce/Assets/Scripts/Interactables/LevelDoor.cs
+++ sp4ce/Assets/Scripts/Interactables/LevelDoor.cs
@@ -8,23 +8,39 @@ public class LevelDoor : MonoBehaviour, IInteract
     [SerializeField] private Transform closePosition;
     [SerializeField] private Transform openPosition;
 
+    private BoxCollider collider;
+
     private bool open;
 
     private void Start()
     {
+        collider = GetComponent<BoxCollider>();
         open = startOpen;
+
         if (open)
+        {
             door.position = openPosition.position;
+            collider.center = openPosition.localPosition;
+        }
         else
+        {
             door.position = closePosition.position;
+            collider.center = closePosition.localPosition;
+        }
     }
 
     private void Update()
     {
         if (open)
+        {
             door.position = Vector3.Slerp(door.position, openPosition.position, openSpeed);
+            collider.center = Vector3.Slerp(collider.center, openPosition.localPosition, openSpeed);
+        }
         else
+        {
             door.position = Vector3.Slerp(door.position, closePosition.position, openSpeed);
+            collider.center = Vector3.Slerp(collider.center, closePosition.localPosition, openSpeed);
+        }
     }
 
     private void ToggleDoor()
