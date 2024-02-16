8df40cbefc03c5c60def86d04b569b6f4f6f5d95
Lee Chii Shian <125229145+Faanggg@users.noreply.github.com>
2024-02-16 16:21:21 +0800

    Bake

--- sp4ce/Assets/Scripts/Enemies/EnemyAnti.cs
+++ sp4ce/Assets/Scripts/Enemies/EnemyAnti.cs
@@ -1,34 +1,66 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAnti : EnemyBase
 {
+    [Header("Anti-NV")]
+    [SerializeField] private Material material;
+    [SerializeField] private float rageBuildUp = 1f;
+
     private enum State
     {
         IDLE,
-        KILL,
+        RAGE,
     }
 
     private State currState;
+    private float rage;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        
-        currState = State.IDLE;
+
+        ChangeState(State.IDLE);
     }
 
     private void Update()
+    {
+        FSM();
+    }
+
+    private void ChangeState(State newState)
+    {
+        if (newState == State.IDLE)
+        {
+            rage = 0f;
+            material.SetFloat("_REFLECTIONS_WEIGHT", rage);
+        }
+        else if (newState == State.RAGE)
+        {
+            
+        }
+
+        currState = newState;
+    }
+
+    public override void FSM()
     {
         if (currState == State.IDLE)
         {
             Vector3 direction = target.position - transform.position;
             transform.forward = direction;
+
+            rage += rageBuildUp * Time.deltaTime;
+            material.SetFloat("_REFLECTIONS_WEIGHT", rage);
+
+            if (rage >= 50f)
+                ChangeState(State.RAGE);
         }
-        else if (currState == State.KILL)
+        else if (currState == State.RAGE)
         {
             Move(target);
         }
@@ -37,10 +69,5 @@ public class EnemyAnti : EnemyBase
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
-    }
-
-    public override void FSM()
-    {
-        
     }
 }
