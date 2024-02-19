87d166c61bd1775421633150a402d779d34bbde1
andrewvil <220518Y@mymail.nyp.edu.sg>
2024-02-19 13:38:04 +0800

    demoscene

--- sp4ce/Assets/Scripts/DoorLever.cs
+++ sp4ce/Assets/Scripts/DoorLever.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorLever : MonoBehaviour, IInteract
+public class DoorLever : MonoBehaviour, IInteract, ISightObserver
 {
     [SerializeField]
     private MultiLeverDoor doorTarget;
+
+    [SerializeField]
+    private MeshRenderer mr;
+
+    [SerializeField]
+    private Light lightsrc;
 
     bool isOn;
 
     void Awake()
     {
         isOn = false;
+        mr.material.color = Color.white;
+        lightsrc.enabled = false;
     }
 
     public string GetItemName()
@@ -29,5 +37,20 @@ public class DoorLever : MonoBehaviour, IInteract
         isOn = !isOn;
         if(isOn) doorTarget.AddPower();
         else doorTarget.RemovePower();
+        mr.material.color = isOn?Color.green:Color.white;
+        lightsrc.enabled = isOn;
+    }
+
+    public void OnSighted()
+    {
+    }
+
+    public void OnLookAway()
+    {
+    }
+
+    public string GetDetails()
+    {
+        return "Lever";
     }
 }
