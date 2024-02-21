27117b107ab2a1611da4b7c453b31cc0e6e8b401
andrewvil <220518Y@mymail.nyp.edu.sg>
2024-02-21 09:53:58 +0800

    lol

--- sp4ce/Assets/Nino_map/Map.cs
+++ sp4ce/Assets/Nino_map/Map.cs
@@ -32,6 +32,7 @@ public class Map : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.M))
         {
             map.SetActive(!map.activeSelf);
+            Cursor.lockState = map.activeSelf?CursorLockMode.None:CursorLockMode.Locked;
             GameManager.instance.isInUI = map.activeSelf;
         }
 
@@ -45,15 +46,17 @@ public class Map : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            Cursor.lockState = CursorLockMode.Locked;
             float mouseX, mouseY;
-            mouseX = Input.GetAxis("Mouse X");
-            mouseY = Input.GetAxis("Mouse Y");
+            mouseX = -Input.GetAxis("Mouse X");
+            mouseY = -Input.GetAxis("Mouse Y");
             Debug.Log("mouseX: " + mouseX + ", mouseY: " + mouseY);
             Vector3 moveDir = mapCamera.transform.up * mouseY + mapCamera.transform.right * mouseX;
             mapCamera.transform.position += new Vector3(moveDir.x, 0, moveDir.z) * Time.deltaTime * 15.0f;
         }
         else if (Input.GetMouseButtonUp(0))
         {
+            Cursor.lockState = CursorLockMode.None;
             Vector3 newPos = Camera.main.transform.position;
             newPos.y = transform.position.y;
             transform.position = newPos;
