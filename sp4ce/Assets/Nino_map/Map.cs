38ddd015be9a085d483fba89209a00aeb77b2030
idsoid <125327978+idsoid@users.noreply.github.com>
2024-02-21 09:27:04 +0800

    update map

--- sp4ce/Assets/Nino_map/Map.cs
+++ sp4ce/Assets/Nino_map/Map.cs
@@ -13,20 +13,54 @@ public class Map : MonoBehaviour
     void Start()
     {
         mapCamera = GetComponent<Camera>();
-        mapMaxSize = 90.0f;
+        mapMaxSize = 110.0f;
         map.SetActive(false);
+
+        Vector3 newPos = Camera.main.transform.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MapEnable();
+    }
+
+    private void MapEnable()
+    {
         if (Input.GetKeyDown(KeyCode.M))
         {
             map.SetActive(!map.activeSelf);
-            GameManager.instance.isInUI= map.activeSelf;
+            GameManager.instance.isInUI = map.activeSelf;
         }
 
-        //Zoom
+        if (map.activeSelf)
+        {
+            MapDisplay();
+            Zoom();
+        }
+    }
+    private void MapDisplay()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX, mouseY;
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
+            Debug.Log("mouseX: " + mouseX + ", mouseY: " + mouseY);
+            Vector3 moveDir = mapCamera.transform.up * mouseY + mapCamera.transform.right * mouseX;
+            mapCamera.transform.position += new Vector3(moveDir.x, 0, moveDir.z) * Time.deltaTime * 15.0f;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 newPos = Camera.main.transform.position;
+            newPos.y = transform.position.y;
+            transform.position = newPos;
+        }
+    }
+    private void Zoom()
+    {
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             if (mapCamera.orthographicSize + 0.5f < mapMaxSize)
@@ -49,10 +83,5 @@ public class Map : MonoBehaviour
                 mapCamera.orthographicSize = 15.0f;
             }
         }
-
-        //Camera update
-        Vector3 newPos = Camera.main.transform.position;
-        newPos.y = transform.position.y;
-        transform.position = newPos;
     }
 }
